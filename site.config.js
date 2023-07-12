fa26e387e957a2d1bb3cda753ffa020a64414e6b
Sinyaya Dynya <sinyayadynya@gmail.com>
2023-07-12 17:30:14 +0600

    Create site.config.js

---
+++ site.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    name: 'Cluster',
+    slogan: 'Cluster',
+    baseUrl: process.env.NEXT_PUBLIC_BASE_URL,
+    drupalBaseUrl: process.env.NEXT_PUBLIC_DRUPAL_BASE_URL,
+    defaultLocale: 'en',
+    locales: {
+        en: 'English',
+        ru: 'Русский',
+        // ky: 'Kyrgyz',
+    },
+};
