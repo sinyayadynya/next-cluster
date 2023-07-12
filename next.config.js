5e60d4e2608299df749d575d45852edf00961a68
Sinyaya Dynya <sinyayadynya@gmail.com>
2023-07-12 17:30:27 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: [process.env.NEXT_IMAGE_DOMAIN],
-  },
-}
 
-module.exports = nextConfig
+const { i18n } = require('./next-i18next.config');
+
+module.exports = {
+    i18n: {
+        defaultLocale: 'en',
+        locales: ['en', 'ru'],
+    },
+    images: {
+        domains: [
+            'source.unsplash.com',
+            'yt3.ggpht.com',
+            'i.ytimg.com',
+            process.env.NEXT_IMAGE_DOMAIN,
+        ],
+    },
+};
