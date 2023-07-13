5ea632e9a8aaa0b46bdf59f0802560073e9abf6d
Sinyaya Dynya <sinyayadynya@gmail.com>
2023-07-13 15:02:41 +0600

    Update media--legislation_object.tsx

--- components/media--legislation_object.tsx
+++ components/media--legislation_object.tsx
@@ -1,27 +1,42 @@
 import { DrupalMedia } from "next-drupal";
 
-interface MediaLegislationObjectProps {
+interface LegislationObjectProps {
   media: DrupalMedia;
 }
 
-export function MediaLegislationObject({ media, ...props }: MediaLegislationObjectProps) {
-  return (
-    <div {...props}>
-      {media.legislation_object && (
-        <div className="mb-4">
-          <h3 className="sr-only">Document</h3>
-          {/* file */}
-          <pre>{JSON.stringify(media.legislation_object, null, 2)}</pre>
-        </div>
-      )}
+function FileLink({ file, name }) {
+    const baseUrl = process.env.NEXT_PUBLIC_DRUPAL_BASE_URL;
+    const fileUrl = `${baseUrl}${file.uri.url}`;
 
-      {media.content_url && (
-        <div className="mb-4">
-          <h3 className="mb-1 text-2xl">Content URL</h3>
-          {/* file */}
-          <pre>{JSON.stringify(media.content_url, null, 2)}</pre>
-        </div>
-      )}
-    </div>
-  );
-}
+    return (
+      <div>
+        <a href={fileUrl} download>
+          {name || file.filename}
+        </a>
+      </div>
+    );
+  }
+
+
+
+export function LegislationObject({ media, ...props }: LegislationObjectProps) {
+    return (
+      <div {...props}>
+        {media.field_media_file && (
+          <div className="mb-4">
+            <h3 className="sr-only">Document</h3>
+            {/* file */}
+            <FileLink file={media.field_media_file} name={media.name} />
+          </div>
+        )}
+
+        {media.content_url && (
+          <div className="mb-4">
+            <h3 className="mb-1 text-2xl">Content URL</h3>
+            {/* file */}
+            <FileLink file={media.content_url} name={media.name} />
+          </div>
+        )}
+      </div>
+    );
+  }
